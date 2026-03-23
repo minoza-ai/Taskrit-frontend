7c0ba306134286aae9134c31ce89ad43c0b60266
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2026-03-23 17:39:13 +0900

    chore: wrap LandingPage in PublicRoute for access control

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<PublicRoute><LandingPage /></PublicRoute>} />
       <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
       <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
 
